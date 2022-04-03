d7f3e58f890c1407d70632c74c57f8f4471eae8b
RichoM <richi.moran@gmail.com>
2022-04-03 17:33:47 -0300

    Building the GUI in cljs

--- src/rsvisualizer/main.cljs
+++ src/rsvisualizer/main.cljs
@@ -1,5 +1,13 @@
 (ns rsvisualizer.main
-  (:require [clojure.core.async :as a :refer [go <!]]))
+  (:require [clojure.core.async :as a :refer [go <!]]
+            [rsvisualizer.ui :as ui]))
+
+(enable-console-print!)
 
 (defn init []
-  (print "RICHO!"))
+  (go (print "RICHO!")
+      (<! (ui/initialize!))))
+
+(defn ^:dev/after-load-async reload-end* [done]
+  (go (<! (ui/initialize!))
+      (done)))
--- src/rsvisualizer/ui.cljs
+++ src/rsvisualizer/ui.cljs
@@ -1,5 +1,54 @@
 (ns rsvisualizer.ui
-  (:require [clojure.core.async :as a :refer [go <!]]))
+  (:require [clojure.core.async :as a :refer [go <!]]
+            [oops.core :refer [oget oset!]]
+            [crate.core :as crate]))
 
-(defn initialize []
-  (go ))
+(defn main-container []
+  (crate/html
+   [:div.container-fluid
+    [:div.row
+     [:div#side-bar.col-auto
+      [:h3.text-center {:style "width: 230px"} "RS Visualizer"]
+      [:div.row.my-1]
+      [:div.d-grid
+       [:button.btn.btn-sm.btn-outline-dark.rounded-pill
+        {:type "button" :data-bs-toggle "button"}
+        [:i.fa-solid.fa-cube.me-2]
+        "Robot 1"]]
+      [:div.row.my-1]
+      [:div.d-grid
+       [:button.btn.btn-sm.btn-outline-dark.rounded-pill
+        {:type "button" :data-bs-toggle "button"}
+        [:i.fa-solid.fa-cube.me-2]
+        "Robot 2"]]
+      [:div.row.my-1]
+      [:div.d-grid
+       [:button.btn.btn-sm.btn-outline-dark.rounded-pill
+        {:type "button" :data-bs-toggle "button"}
+        [:i.fa-solid.fa-cube.me-2]
+        "Robot 3"]]
+      [:div.row.my-2]
+      [:div.form-check.form-switch.text-center.mx-3
+       [:input#ball-prediction.form-check-input {:type "checkbox" :role "switch" :checked true}]
+       [:label.form-check-.ebal {:for "ball-prediction"} "Ball prediction?"]]]
+     [:div#field-panel.col
+      [:div#top-buttons.row.text-center.my-1
+       [:div.col]
+       [:div.col-3
+        [:div.d-grid
+         [:button.btn.btn-sm.btn-outline-dark.rounded-pill {:type "button"}
+          [:i.fa-solid.fa-ruler.me-2]
+          "Measure"]]]
+       [:div.col-3
+        [:div.d-grid
+         [:button.btn.btn-sm.btn-outline-dark.rounded-pill {:type "button"}
+          [:i.fa-solid.fa-arrows-up-down-left-right.me-2]
+          "Move"]]]
+       [:div.col]]
+      [:div.row.bg-dark
+       [:canvas#field-canvas]]]]]))
+
+(defn initialize! []
+  (go (doto js/document.body
+        (oset! :innerHTML "")
+        (.appendChild (main-container)))))
