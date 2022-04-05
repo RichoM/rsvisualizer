8077250939b61b33816eaaba228e6c7063b95343
RichoM <richi.moran@gmail.com>
2022-04-05 12:15:04 -0300

    Added an update loop that moves the robots according to the data from the simulator

--- src/rsvisualizer/ui.cljs
+++ src/rsvisualizer/ui.cljs
@@ -6,6 +6,9 @@
             [utils.pixi :as pixi]
             [utils.core :as u]))
 
+(defonce pixi (atom nil))
+(defonce updates (atom nil))
+
 (def ^:const PIXEL_TO_WORLD (/ 1.5 864))
 
 (defn pixel->world [[x y]]
@@ -62,15 +65,13 @@
        [:div#mouse-pos.position-absolute.start-0.top-0.font-monospace]]]]]
     ))
 
-
-
 (defn initialize-main-ui! []
   (doto js/document.body
     (oset! :innerHTML "")
     (.appendChild (main-container))))
     
-(defn resize-field [state-atom]
-  (let [{:keys [html app field]} (-> @state-atom :pixi)]
+(defn resize-field []
+  (let [{:keys [html app field]} @pixi]
     (doto html
       (oset! :style.height
              (u/format "calc(100% - %1px)"
@@ -103,42 +104,73 @@
         (doto field
           (oset! :interactive true)
           (ocall! :on "mousemove"
-               (fn [e] (let [position (ocall! e :data.getLocalPosition field)
-                             [px py] [(oget position :x) (oget position :y)]
-                             [wx wy] (pixel->world [px py])]
-                         (pixi/set-position! ball (world->pixel 
-                                                   (pixel->world [px py])))
-                         (oset! (js/document.getElementById "mouse-pos")
-                                :innerText (u/format "[%1 %2]"
-                                                     (.toFixed wx 3)
-                                                     (.toFixed wy 3)))))))
-        (swap! state-atom assoc :pixi
-               {:app app
-                :html html
-                :field field
-                :robots robots
-                :ball ball})
-        (.addEventListener js/window "resize" #(resize-field state-atom))
-        (resize-field state-atom))))
+                  (fn [e] (let [position (ocall! e :data.getLocalPosition field)
+                                [px py] [(oget position :x) (oget position :y)]
+                                [wx wy] (pixel->world [px py])]
+                            (swap! state-atom assoc :cursor {:pixel [px py]
+                                                             :world [wx wy]})))))
+        (reset! pixi
+                {:app app
+                 :html html
+                 :field field
+                 :robots robots
+                 :ball ball})
+        (.addEventListener js/window "resize" resize-field)
+        (resize-field))))
+
+(defn update-ui [state]
+  (go 
+    (let [{:keys [ball robots]} @pixi]
+        (when-let [[wx wy] (-> state :cursor :world)]
+          (oset! (js/document.getElementById "mouse-pos")
+                 :innerText (u/format "[%1 %2]"
+                                      (.toFixed wx 3)
+                                      (.toFixed wy 3))))
+        (when-let [snapshot (-> state :latest-snapshot)]
+          ;(js/console.log (clj->js snapshot))
+          (when-let [{:keys [x y stale-time]} (snapshot :ball)]
+            (doto ball
+              (oset! :tint (if (< stale-time 0.1)
+                             0x00ff00
+                             0xaaaaaa))
+              (pixi/set-position! (world->pixel [x y]))))
+          (doseq [[idx {:keys [x y a]}] (map-indexed vector (snapshot :robots))]
+            (doto (nth robots idx)
+              (pixi/set-position! (world->pixel [x y]))
+              (pixi/set-rotation! (* -1 a))))))))
+
+(defn start-update-loop [state-atom]
+  (reset! updates (a/chan (a/sliding-buffer 1)))
+  (add-watch state-atom ::updates 
+             (fn [_ _ _ new-state]
+               (a/put! @updates new-state)))
+  (go (loop []
+        (when-some [update (<! @updates)]
+          (<! (update-ui update))
+          ;(<! (a/timeout 16))
+          (recur)))))
 
 (defn initialize! [state-atom]
   (go (initialize-main-ui!)
-      (<! (initialize-pixi! state-atom))))
+      (<! (initialize-pixi! state-atom))
+      (start-update-loop state-atom)))
 
-(defn terminate! [state-atom]
-  (go (ocall! (-> @state-atom :pixi :app)
+(defn terminate! []
+  (go (a/close! @updates)
+      (ocall! (-> @pixi :app)
               :destroy true {:children true
                              :texture true
                              :baseTexture true})))
 
 (comment
+  (def state-atom rsvisualizer.main/state)
+  (def app (-> @pixi :app))
+  (def field (-> @pixi :field))
+  (def robots (-> @pixi :robots))
+  (def ball (-> @pixi :ball))
   
-  (let [state @rsvisualizer.main/state]
-    (def app (-> state :pixi :app))
-    (def field (-> state :pixi :field))
-    (def robots (-> state :pixi :robots))
-    (def ball (-> state :pixi :ball)))
-
-
+  (doto (first robots)
+    (pixi/set-position! [-200 0])
+    (pixi/set-rotation! 0))
   )
   
