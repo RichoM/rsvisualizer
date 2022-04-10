2a7ff8507dab8efa019cf0ea0cb1b38d2e19b64f
RichoM <richi.moran@gmail.com>
2022-04-10 12:39:54 -0300

    Refactoring pixi initialization

--- src/rsvisualizer/ui.cljs
+++ src/rsvisualizer/ui.cljs
@@ -143,117 +143,144 @@
       (pixi/set-height! (- (oget app :screen.height) 15))
       (pixi/set-position! (pixi/get-screen-center app)))))
 
+(defn initialize-field! [state-atom app {field-texture :field}]
+  (let [field (pixi/make-sprite! field-texture)]
+    (doto field
+      (pixi/set-position! (pixi/get-screen-center app))
+      (pixi/add-to! (oget app :stage))
+      (oset! :interactive true)
+      (ocall! :on "click" (fn [e]
+                            (ocall! e :stopPropagation)
+                            (swap! state-atom assoc :selected-robot nil)))
+      (ocall! :on "mousemove"
+              (fn [e] (let [position (ocall! e :data.getLocalPosition field)
+                            pixel-coords [(oget position :x) (oget position :y)]
+                            world-coords (pixel->world pixel-coords)]
+                        (swap! state-atom assoc :cursor {:pixel pixel-coords
+                                                         :world world-coords})))))))
+
+(defn initialize-previous-ball! [field {ball-texture :ball}]
+  (doto (pixi/make-sprite! ball-texture)
+    (oset! :alpha 0.5)
+    (pixi/set-position! [0 0])
+    (pixi/add-to! field)))
+
+(defn initialize-future-balls! [field {ball-texture :ball}]
+  (mapv (fn [idx]
+          (doto (pixi/make-sprite! ball-texture)
+            (oset! :alpha (+ 0.15 (* idx (/ -0.1 15))))
+            (oset! :tint 0x0000ff)
+            (pixi/set-position! [0 0])
+            (pixi/add-to! field)))
+        (range 15)))
+
+(defn initialize-ball! [field {ball-texture :ball}]
+  (doto (pixi/make-sprite! ball-texture)
+    (oset! :tint 0x00ff00)
+    (pixi/set-position! [0 0])
+    (pixi/add-to! field)))
+
+(defn initialize-robots! [state-atom app field {robot-texture :robot}]
+  (let [label-style (js/PIXI.TextStyle. (clj->js {:fontFamily "sans-serif"
+                                                  :fontSize 26
+                                                  :fontWeight "bold"
+                                                  :fill "#ffffff"
+                                                  :stroke "#000000"
+                                                  :strokeThickness 4}))
+        add-robot-label! (fn [robot idx]
+                           (let [label (pixi/make-label! idx label-style)]
+                             (pixi/add-child! field label)
+                             (pixi/add-ticker! app
+                                               #(pixi/set-position! label [(oget robot :x)
+                                                                           (oget robot :y)]))))]
+    (mapv (fn [idx]
+            (doto (pixi/make-sprite! robot-texture)
+              (oset! :tint GRAY_REGULAR)
+              (oset! :interactive true)
+              (ocall! :on "click" (fn [e]
+                                    (ocall! e :stopPropagation)
+                                    (swap! state-atom update :selected-robot
+                                           #(if (= % idx) nil idx))))
+              (pixi/set-position! [(* 50 (dec idx)) -50])
+              (pixi/add-to! field)
+              (add-robot-label! (inc idx))))
+          (range 3))))
+
+(defn initialize-roles! [app field robots]
+  (let [label-style (js/PIXI.TextStyle. (clj->js {:fontFamily "sans-serif"
+                                                  :fontSize 16
+                                                  :fill "#000000"
+                                                  :stroke "#ffffff"
+                                                  :strokeThickness 2}))]
+    (mapv (fn [robot]
+            (let [label (doto (pixi/make-label! "" label-style)
+                          (pixi/add-to! field))]
+              (pixi/add-ticker! app #(let [x (oget robot :x)
+                                           y (if (< (oget robot :y)
+                                                    (/ (oget field :height) -2))
+                                               (+ (oget robot :y) 40)
+                                               (+ (oget robot :y) -40))]
+                                       (pixi/set-position! label [x y])))
+              label))
+          robots)))
+
+(defn initialize-targets! [app field robots {cross-texture :cross}]
+  (mapv (fn [robot]
+          (let [target (doto (pixi/make-sprite! cross-texture)
+                         (oset! :visible false)
+                         (pixi/add-to! field))
+                line (doto (js/PIXI.Graphics.)
+                       (pixi/add-to! field))]
+            (pixi/add-ticker! app #(if (oget target :visible)
+                                     (doto line
+                                       (ocall! :clear)
+                                       (ocall! :lineStyle (clj->js {:width 2
+                                                                    :color 0x5555ff
+                                                                    :alpha 0.5}))
+                                       (pixi/draw-dashed-line! [(oget robot :x) (oget robot :y)]
+                                                               [(oget target :x) (oget target :y)]
+                                                               :gap 5)
+                                       (ocall! :closePath)
+                                       (oset! :visible true))
+                                     (oset! line :visible false)))
+            target))
+        robots))
+
+(defn initialize-cursor! [state-atom app field]
+  (let [label-style (js/PIXI.TextStyle. (clj->js {:fontFamily "monospace"
+                                                  :fontSize 22
+                                                  :stroke "#ffffff"
+                                                  :fill "#656565"
+                                                  :strokeThickness 5}))
+        label (doto (pixi/make-label! "" label-style)
+                (oset! :anchor.x 0.0))]
+    (pixi/add-child! field label)
+    (pixi/add-ticker! app #(when-let [{[wx wy] :world [px py] :pixel}
+                                      (-> @state-atom :cursor)]
+                             (oset! label :text (u/format "[%1 %2]"
+                                                          (.toFixed wx 3)
+                                                          (.toFixed wy 3)))
+                             (pixi/set-position! label [(+ px 12) py])))
+    label))
+
+(defn load-textures! []
+  (go {:field (<! (pixi/load-texture! "imgs/field.png"))
+       :ball (<! (pixi/load-texture! "imgs/ball.png"))
+       :robot (<! (pixi/load-texture! "imgs/robot.png"))
+       :cross (<! (pixi/load-texture! "imgs/cross.png"))}))
 
 (defn initialize-pixi! [state-atom]
   (go (let [html (js/document.getElementById "field-panel")
             app (pixi/make-application! html)
-            field (doto (pixi/make-sprite! (<! (pixi/load-texture! "imgs/field.png")))
-                    (pixi/set-position! (pixi/get-screen-center app))
-                    (pixi/add-to! (oget app :stage)))
-            ball-texture (<! (pixi/load-texture! "imgs/ball.png"))
-            previous-ball (doto (pixi/make-sprite! ball-texture)
-                            (oset! :alpha 0.5)
-                            (pixi/set-position! [0 0])
-                            (pixi/add-to! field))
-            future-balls (mapv (fn [idx]
-                                 (doto (pixi/make-sprite! ball-texture)
-                                   (oset! :alpha (+ 0.15 (* idx (/ -0.1 15))))
-                                   (oset! :tint 0x0000ff)
-                                   (pixi/set-position! [0 0])
-                                   (pixi/add-to! field)))
-                               (range 15))
-            ball (doto (pixi/make-sprite! ball-texture)
-                   (oset! :tint 0x00ff00)
-                   (pixi/set-position! [0 0])
-                   (pixi/add-to! field))
-            robots (let [robot-texture (<! (pixi/load-texture! "imgs/robot.png"))
-                         label-style (js/PIXI.TextStyle. (clj->js {:fontFamily "sans-serif"
-                                                                   :fontSize 26
-                                                                   :fontWeight "bold"
-                                                                   :fill "#ffffff"
-                                                                   :stroke "#000000"
-                                                                   :strokeThickness 4}))]
-                     (mapv (fn [idx]
-                             (let [add-robot-label! (fn [robot idx]
-                                                      (let [label (pixi/make-label! idx label-style)
-                                                            ticker #(pixi/set-position! label [(oget robot :x)
-                                                                                               (+ (oget robot :y) 0)])]
-                                                        (pixi/add-child! field label)
-                                                        (pixi/add-ticker! app ticker)))]
-                               (doto (pixi/make-sprite! robot-texture)
-                                 (oset! :tint GRAY_REGULAR)
-                                 (oset! :interactive true)
-                                 (ocall! :on "click" (fn [e]
-                                                       (ocall! e :stopPropagation)
-                                                       (swap! state-atom update :selected-robot
-                                                              #(if (= % idx) nil idx))))
-                                 (pixi/set-position! [(* 50 (dec idx)) -50])
-                                 (pixi/add-to! field)
-                                 (add-robot-label! (inc idx)))))
-                           (range 3)))
-            roles (let [label-style (js/PIXI.TextStyle. (clj->js {:fontFamily "sans-serif"
-                                                                  :fontSize 16
-                                                                  :fill "#000000"
-                                                                  :stroke "#ffffff"
-                                                                  :strokeThickness 2}))]
-                    (mapv (fn [robot]
-                            (let [label (doto (pixi/make-label! "" label-style)
-                                          (pixi/add-to! field))]
-                              (pixi/add-ticker! app #(let [x (oget robot :x)
-                                                           y (if (< (oget robot :y)
-                                                                    (/ (oget field :height) -2))
-                                                               (+ (oget robot :y) 40)
-                                                               (+ (oget robot :y) -40))]
-                                                       (pixi/set-position! label [x y])))
-                              label))
-                          robots))
-            targets (let [cross-texture (<! (pixi/load-texture! "imgs/cross.png"))]
-                      (mapv (fn [robot]
-                              (let [target (doto (pixi/make-sprite! cross-texture)
-                                             (oset! :visible false)
-                                             (pixi/add-to! field))
-                                    line (doto (js/PIXI.Graphics.)
-                                           (pixi/add-to! field))]
-                                (pixi/add-ticker! app #(if (oget target :visible)
-                                                         (doto line
-                                                           (ocall! :clear)
-                                                           (ocall! :lineStyle (clj->js {:width 2
-                                                                                        :color 0x5555ff
-                                                                                        :alpha 0.5}))
-                                                           (pixi/draw-dashed-line! [(oget robot :x) (oget robot :y)]
-                                                                                   [(oget target :x) (oget target :y)]
-                                                                                   :gap 5)
-                                                           (ocall! :closePath)
-                                                           (oset! :visible true))
-                                                         (oset! line :visible false)))
-                                target))
-                            robots))
-            cursor (let [label-style (js/PIXI.TextStyle. (clj->js {:fontFamily "monospace"
-                                                                   :fontSize 22
-                                                                   :stroke "#ffffff"
-                                                                   :fill "#656565"
-                                                                   :strokeThickness 5}))
-                         label (doto (pixi/make-label! "" label-style)
-                                 (oset! :anchor.x 0.0))]
-                     (pixi/add-child! field label)
-                     (pixi/add-ticker! app #(when-let [{[wx wy] :world [px py] :pixel}
-                                                       (-> @state-atom :cursor)]
-                                              (oset! label :text (u/format "[%1 %2]"
-                                                                           (.toFixed wx 3)
-                                                                           (.toFixed wy 3)))
-                                              (pixi/set-position! label [(+ px 12) py])))
-                     label)]
-        (doto field
-          (oset! :interactive true)
-          (ocall! :on "click" (fn [e]
-                                (ocall! e :stopPropagation)
-                                (swap! state-atom assoc :selected-robot nil)))
-          (ocall! :on "mousemove"
-                  (fn [e] (let [position (ocall! e :data.getLocalPosition field)
-                                pixel-coords [(oget position :x) (oget position :y)]
-                                world-coords (pixel->world pixel-coords)]
-                            (swap! state-atom assoc :cursor {:pixel pixel-coords
-                                                             :world world-coords})))))
+            textures (<! (load-textures!))
+            field (initialize-field! state-atom app textures)
+            previous-ball (initialize-previous-ball! field textures)
+            future-balls (initialize-future-balls! field textures)
+            ball (initialize-ball! field textures)
+            robots (initialize-robots! state-atom app field textures)
+            roles (initialize-roles! app field robots)
+            targets (initialize-targets! app field robots textures)
+            cursor (initialize-cursor! state-atom app field)]
         (reset! pixi
                 {:app app
                  :html html
